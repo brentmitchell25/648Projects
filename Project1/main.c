30428785bd3068a123af0f88b4287d944d820695
Brent Mitchell <brent.mitchell25@gmail.com>
2015-03-01 00:21:34 -0600

    cd now works correctly

--- Project1/main.c
+++ Project1/main.c
@@ -12,50 +12,59 @@
 
 #define MAX_LINE		80 /* 80 chars per line, per command */
 #define DELIMS                  " \t\r\n"
-int main(int argc, char **argv, char **envp)
-{
-  char args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
-  char* string[MAX_LINE];
+int main(int argc, char **argv, char **envp) {
+	char args[MAX_LINE / 2 + 1]; /* command line (of 80) has max of 40 arguments */
+	char* string[MAX_LINE];
 
-  pid_t child_id;
-  int should_run = 1;
-  char *cmd;
-  int i, upper;
-  while (should_run){   
-    printf("quash> ");
-    fflush(stdout);
-    if(!fgets(args,MAX_LINE,stdin)) break;
-    cmd = strtok(args,DELIMS);
-    int i =0;
-    if(!strcmp(cmd,"exit") || !strcmp(cmd,"quit"))
-      return 0;
+	pid_t child_id;
+	int should_run = 1;
+	char *cmd;
+	int i, upper, should_fork;
+	while (should_run) {
+		should_fork = 1;
+		printf("quash> ");
+		fflush(stdout);
+		if (!fgets(args, MAX_LINE, stdin))
+			break;
+		cmd = strtok(args, DELIMS);
+		int i = 0;
+		if (!strcmp(cmd, "exit") || !strcmp(cmd, "quit"))
+			return 0;
 
-    if(strpbrk(cmd,"cd") != NULL) {
-      chdir(argv[]);
-    }
-    while(cmd != NULL){
-      string[i] = cmd;
-      cmd = strtok(NULL,DELIMS);
-      i++;
-    }
-    string[i] = NULL;
-    pid_t pid = fork();
-    if(pid == 0) {
-      child_id = getpid();
-      execvp(string[0],string);
-      printf("Unknown command\n");
-      exit(0);
-    } else {
-      wait(NULL);
-    }
+		while (cmd != NULL) {
+			string[i] = cmd;
+			cmd = strtok(NULL, DELIMS);
+			i++;
+		}
 
-    /**
-     * After reading user input, the steps are:
-     * (1) fork a child process
-     * (2) the child process will invoke execvp()
-     * (3) if command included &, parent will invoke wait()
-     */
-  }
-    
-  return 0;
+		if (strpbrk(string[0], "cd") != NULL) {
+			chdir(string[1]);
+			puts("HERE");
+			should_fork = 0;
+		}
+
+		string[i] = NULL;
+
+		// Don't fork if running built in function
+		if (should_fork) {
+			pid_t pid = fork();
+			if (pid == 0) {
+				child_id = getpid();
+				execvp(string[0], string);
+				printf("Unknown command\n");
+				exit(0);
+			} else {
+				wait(NULL);
+			}
+		}
+
+		/**
+		 * After reading user input, the steps are:
+		 * (1) fork a child process
+		 * (2) the child process will invoke execvp()
+		 * (3) if command included &, parent will invoke wait()
+		 */
+	}
+
+	return 0;
 }
